99bb7e0ae2ab456f9bc5434c713fb33cf0a55351
aspectZA27 <ruakruger@gmail.com>
2026-06-24 21:50:29 +0200

    add more features and refine design

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+let letterA = document.getElementById("letterChangeA");
 
+const letters = "A4A4A4".split("");
+let index = 0;
+let isVisible = true;
+
+setInterval(() => {
+  letterA.textContent = letters[index];
+  index = (index + 1) % letters.length;
+
+  setInterval(() => {
+    isVisible = !isVisible;
+    letterA.style.opacity = isVisible ? "1" : "0";
+  }, 300);
+}, 2000);
