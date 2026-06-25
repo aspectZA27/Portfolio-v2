6fea7423b63a72d3bf8ff42854189baf84b73c13
aspectZA27 <ruakruger@gmail.com>
2026-06-25 22:34:22 +0200

    Finish title

--- index.js
+++ index.js
@@ -1,15 +1,68 @@
-let letterA = document.getElementById("letterChangeA");
+const ruan = "RUAN";
+let ruan1 = document.getElementById("titleR");
+let ruanIndex = 0;
+const ruanAppear = setInterval(() => {
+  let span = document.createElement("span");
+  span.textContent = ruan[ruanIndex];
 
-const letters = "A4A4A4".split("");
-let index = 0;
-let isVisible = true;
+  if (ruanIndex === 2) {
+    span.id = "letterA";
+  }
+
+  ruan1.appendChild(span);
+  ruanIndex++;
+
+  if (ruanIndex >= ruan.length) {
+    clearInterval(ruanAppear);
+
+    let letterA = document.getElementById("letterA");
+
+    const kruger = "KRUGER";
+    let kruger1 = document.getElementById("titleK");
+    let krugerIndex = 0;
+    const krugerAppear = setInterval(() => {
+      let span = document.createElement("span");
+      span.textContent = kruger[krugerIndex];
+
+      if (krugerIndex === 4) {
+        span.id = "letterE";
+      }
+
+      kruger1.appendChild(span);
+      krugerIndex++;
+
+      if (krugerIndex >= kruger.length) {
+        clearInterval(krugerAppear);
+
+        let letterE = document.getElementById("letterE");
+      }
+    }, 200);
+  }
+}, 200);
+
+const lettersA = ["A", "4"];
+const lettersE = ["E", "3"];
+let indexA = 0;
+let indexE = 0;
+let isVisibleA = true;
+let isVisibleE = true;
 
 setInterval(() => {
-  letterA.textContent = letters[index];
-  index = (index + 1) % letters.length;
+  letterA.textContent = lettersA[indexA];
+  indexA = (indexA + 1) % lettersA.length;
 
   setInterval(() => {
-    isVisible = !isVisible;
-    letterA.style.opacity = isVisible ? "1" : "0";
+    isVisibleA = !isVisibleA;
+    letterA.style.opacity = isVisibleA ? "1" : "0";
   }, 300);
+}, 2000);
+
+setInterval(() => {
+  letterE.textContent = lettersE[indexE];
+  indexE = (indexE + 1) % lettersE.length;
+
+  setInterval(() => {
+    isVisibleE = !isVisibleE;
+    letterE.style.opacity = isVisibleE ? "1" : "0";
+  }, 500);
 }, 2000);
